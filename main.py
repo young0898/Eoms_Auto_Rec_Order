a0a32ce81fc3c6f57df4b1d8584268d6052842d9
zhangyang <zhangyang1@hi.chinamobile.com>
2021-02-24 17:34:33 +0800

    Initial commit

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from common.timer import Timer
 from spider.spider import Spider
 
 if __name__ == '__main__':
-    a = """功能列表：                                                                                
-    1、登录OA，保存cookies
-    2、跳转方式登录EMOS
-    """
-    #print(a)
 
     spider = Spider()
     timer = Timer()
